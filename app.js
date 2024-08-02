a79dcf04459dbd6c755ba1b5a9fceefb0faa6f7a
andradee0 <rafaandra157@gmail.com>
2024-08-02 08:57:22 -0300

    teste

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ import express from 'express';
 
 const servidor = express();
 
+
+servidor.get('/calculadora/somar/:n1/:n2', ( req, resp ) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+    let total = n1 + n2;
+
+    resp.send('O total é ' + total);
+
+})
+
+
 servidor.listen(5050,
      () => console.log('API haahahahah'));
