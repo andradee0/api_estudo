9bc6b876113da7ada492267cbfedfed5220c9a16
matheuslacerdaaaa <matheus301107@gmail.com>
2024-08-02 09:16:25 -0300

    oii

--- app.js
+++ app.js
@@ -13,5 +13,20 @@ servidor.get('/calculadora/somar/:n1/:n2', ( req, resp ) => {
 })
 
 
-servidor.listen(5050,
+
+servidor.get('/calculadora/subtrair/:n1/:n2', (req, resp) => {
+
+
+let n1 = Number (req.params.n1)
+let n2 = Number (req.params.n2)
+
+let ret = n1 - n2
+
+resp.send("o rsultado é " + ret)
+
+
+} )
+
+
+servidor.listen(5002,
      () => console.log('API haahahahah'));
